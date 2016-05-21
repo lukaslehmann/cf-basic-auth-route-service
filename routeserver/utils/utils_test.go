33488882d0bc42bc09d26b3a6042419414ee1194
Ben Laplanche <ben@laplanche.co.uk>
2016-05-21 16:30:51 +0100

    added more tests cases

--- routeserver/utils/utils_test.go
+++ routeserver/utils/utils_test.go
@@ -8,16 +8,28 @@ import (
 )
 
 var _ = Describe("Utils", func() {
-	var simpleURL string
-
-	BeforeEach(func() {
-		simpleURL = "https://myapp.pcf.io"
-	})
+	var example1 string = "https://myapp.pcf.io"
+	var example2 string = "http://my-app.pcf.io"
+	var example3 string = "https://my-app-1.pcf.io"
 
 	Describe(".Strip Special characters and reverse the string", func() {
 		Context(".without special characters", func() {
 			It("should correctly reverse the string", func() {
-				result := utils.StripAndReverse(simpleURL)
+				result := utils.StripAndReverse(example1)
+
+				Expect(result).To(Equal("ppaym"))
+			})
+
+			It("should work with numbers", func() {
+				result := utils.StripAndReverse(example3)
+				Expect(result).To(Equal("1ppaym"))
+			})
+
+		})
+
+		Context(".with special characters", func() {
+			It("should correctly reverse the string", func() {
+				result := utils.StripAndReverse(example2)
 
 				Expect(result).To(Equal("ppaym"))
 			})

312cb7b32706fec671dd5192c54c4a7a46ed6564
Ben Laplanche <ben@laplanche.co.uk>
2016-05-21 17:05:33 +0100

    started building the proxy packge

---
+++ routeserver/proxy/proxy_test.go
@@ -0,0 +1,12 @@
+package proxy_test
+
+import (
+	. "github.com/benlaplanche/cf-basic-auth-route-service/routeserver/proxy"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Proxy", func() {
+
+})

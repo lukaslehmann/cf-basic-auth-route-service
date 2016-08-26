e347da79ce50b23ba0be9169fe2cadedd468ed88
Ben Laplanche <ben@laplanche.co.uk>
2016-08-26 18:13:33 +0100

    added some tests cases for IP addresses

--- routeserver/utils/utils_test.go
+++ routeserver/utils/utils_test.go
@@ -11,6 +11,8 @@ var _ = Describe("Utils", func() {
 	var example1 string = "https://myapp.pcf.io"
 	var example2 string = "http://my-app.pcf.io"
 	var example3 string = "https://my-app-1.pcf.io"
+	var example4 string = "http://127.0.0.1"
+	var example5 string = "https://127.0.0.1:8080"
 
 	Describe(".Strip Special characters and reverse the string", func() {
 		Context(".without special characters", func() {
@@ -32,6 +34,18 @@ var _ = Describe("Utils", func() {
 				result := utils.StripAndReverse(example2)
 
 				Expect(result).To(Equal("ppaym"))
+			})
+
+			It("should correctly reverse the IP address", func() {
+				result := utils.StripAndReverse(example4)
+
+				Expect(result).To(Equal("721"))
+			})
+
+			It("should correctly reverse the IP address when it also has a port", func() {
+				result := utils.StripAndReverse(example5)
+
+				Expect(result).To(Equal("721"))
 			})
 		})
 	})
